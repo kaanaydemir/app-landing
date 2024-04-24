7834decc44a4c29b372f59f7f2938e7ce8afa08f
kaanaydemir <kaan.aydemir.iu@gmail.com>
2024-04-24 19:06:37 +0300

    Hover effect added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -29,13 +29,13 @@ function Home(props) {
 
 
   return (
-    <div className="flex flex-col flex-1 slider-container mx-40 justify-center items-center">
+    <div className="flex flex-col flex-1 mx-40 justify-center items-center">
       <div className={"flex justify-center"}>
-        <Slider slideImages={slideImages}/>
-        <div className={"text-white ml-20 w-2/6 text-left"}>
+        <div className={"hidden sm:block"}><Slider slideImages={slideImages}/></div>
+        <div className={"text-center sm:text-left text-white sm:ml-20 ml-0 w-full sm:w-2/6"}>
           <h1 className={"font-bold text-5xl kanit-bold"}>Tuwenti.com</h1>
           <p className={"my-4 kanit-semibold"}>Elevating Horizons, Building Dreams</p>
-          <section className={"kanit-extralight text-sm space-y-2 text-stone-200"}>
+          <section className={"kanit-extralight text-sm text-left space-y-2 text-stone-200"}>
             <p>At tuwenti, we pride ourselves on developing applications that simplify life's complexities—whether it's
               document transformation challenges, latest earthquake tracking, or ensuring personal safety.</p>
             <p>Our focus is on delivering top-notch solutions to problems in our daily life's.</p>
